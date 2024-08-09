03c836a10f2b38ec1dea58b8492d669bd5d88937
Chere3 <71246795+Chere3@users.noreply.github.com>
2024-08-09 15:27:51 -0600

    update fish.nix with shell aliases and greeting function

--- home/fish.nix
+++ home/fish.nix
@@ -1,8 +1,12 @@
 {
-    enable = true;
-    shellAliases = {
-        sho = "home-manager switch";
-        rho = "home-manager switch --rollback";
-        sno = "sudo nixos-rebuild switch";
-    };
+  enable = true;
+  shellAliases = {
+    sho = "home-manager switch";
+    rho = "home-manager switch --rollback";
+    sno = "sudo nixos-rebuild switch";
+  };
+  shellInit = ''
+    function fish_greeting
+    end
+  '';
 }
