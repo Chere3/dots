2da3445aba56d8dd799d385683f77dc2013dd907
Chere3 <71246795+Chere3@users.noreply.github.com>
2024-10-23 16:48:15 -0600

    fix(nixvim): Fix typo in colorscheme name

--- home/cheree/calypso/cli/nixvim.nix
+++ home/cheree/calypso/cli/nixvim.nix
@@ -3,7 +3,7 @@ let inherit (inputs) nixvim; in {
   imports = [ nixvim.homeManagerModules.nixvim ];
   programs.nixvim = {
     enable = true;
-    colorschemes.catpuccin.enable = true;
+    colorschemes.catppuccin.enable = true;
     plugins.lualine.enable = true;
   };
 }
